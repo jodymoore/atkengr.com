fda3915ca6252b432ee17bc2752a2bd0ef248ec1
jody moore <jodymoore@jodys-Mac-mini-2.local>
2019-02-23 16:00:13 -0600

    new commits

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,14 +3,14 @@
 @section('content')
 
 @include('header')
-
+<br>
 <div id="indexPageDiv">
 
-   <h2 class="mainFont" >DETAILED PROCEDURES & TUTORIALS</h2><br>
+   <h2 class="mainFont" >DETAILED PROCEDURES & TUTORIALS</h2>
    <h2 class="mainFont" >USING AUTOCAD CIVIL 3D</h2>
+<br>
 
-
-   <p>Gerald Atkins is a Proffessional Engineer with over 55 years of experience who has spent over 5 years in the development of a comprehensive set of Step-by-Step procedures, with Tutorials, on the use of AutoCad 3D in the preperation of Construction Drawings for Road, Drainage, Wastewater, and Water Improvements. The Procedures and Tutorials will be a great benefit to a beginner or experienced user of AutoCad Civil 3D.</p>
+   <p>Gerald Atkins is a Professional Engineer with over 55 years of experience who has spent over 5 years in the development of a comprehensive set of Step-by-Step procedures, with Tutorials, on the use of AutoCad 3D in the preperation of Construction Drawings for Road, Drainage, Wastewater, and Water Improvements. The Procedures and Tutorials will be a great benefit to a beginner or experienced user of AutoCad Civil 3D.</p>
 
 </div>
 
