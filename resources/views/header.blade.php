94b2616ead9a9bcf16edfa3e1c6ab9ea513313ef
jody moore <jodymoore@jodys-Mac-mini-2.local>
2019-02-24 17:49:18 -0600

    new font and style

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,20 +1,25 @@
-<div id="title"> 
+<div id="title" class="d-flex justify-content-center"> 
 <!--  -->
-    <h1 id="titleHeader"><span>A</span>TKINS <span>E</span>NGINEERING, <span>I</span>NC</h1> 
+
+<div id="leftsideDiv"></div>
+      <h1 id="titleHeader"><span>A</span>TKINS <span>E</span>NGINEERING, <span>I</span>NC</h1> 
   
-        <div id="checkout" class="col-xs-4"> 
+        <div id="checkout"> 
          
             <div id="total">
                  
              </div>  
 
-          <ul class="nav navbar-nav navbar-right">
+          <ul class="nav navbar-nav">
               <li><a href="/order">CHECK OUT</a></li>
           </ul>
              
 
      </div>
 
-
 </div>
+
+<br>
+
+
   @include('nav')
